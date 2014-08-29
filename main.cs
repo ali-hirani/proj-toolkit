9ac0c126c237ac2978dee96b26e46f03d39860e8
Ali Hirani <ali123394@gmail.com>
2014-08-29 01:59:14 -0400

    UI Changes to Rock Paper Scissors

--- main.cs
+++ main.cs
@@ -805,12 +805,9 @@ class RockPaperScissors
     public void RPS()
     {
         Random rpsvariable = new Random(); 
-        rpsRando = rpsvariable.Next(100); 
+        rpsRando = rpsvariable.Next(100);
 
-
-        
-        ConsoleKeyInfo rpsKey; 
-
+        ConsoleKeyInfo rpsKey;
 
         if(rpsRando <= 33)
         {
@@ -824,28 +821,31 @@ class RockPaperScissors
         {
             rpsRan = "scissors"; 
         }
-            Console.WriteLine("Do you choose rock, paper or scissors?");
-            Console.WriteLine("");
-            Console.WriteLine("1. Rock");
-            Console.WriteLine("2. Paper");
-            Console.WriteLine("3. Scissors");
-            Console.WriteLine("");
+
+        Console.WriteLine("Do you choose rock, paper or scissors?");
+        Console.WriteLine("");
+        Console.WriteLine("1. Rock");
+        Console.WriteLine("2. Paper");
+        Console.WriteLine("3. Scissors");
+        Console.WriteLine("");
+        
         do
         {
-
             rpsKey = Console.ReadKey();
             if(rpsKey.KeyChar == '1')
             {
-                rpsUser = "rock"; 
+                rpsUser = "rock";
+                Console.WriteLine("---Accepted");
             }
             else if(rpsKey.KeyChar == '2')
             {
                 rpsUser = "paper"; 
-
+                Console.WriteLine("---Accepted");
             }
             else if(rpsKey.KeyChar == '3')
             {
-                rpsUser = "scissors"; 
+                rpsUser = "scissors";
+                Console.WriteLine("---Accepted");
             }
             else
             {
@@ -854,57 +854,57 @@ class RockPaperScissors
         }
         while(rpsKey.KeyChar != '1' && rpsKey.KeyChar != '2' && rpsKey.KeyChar != '3');
 
-        Console.WriteLine(""); 
+        Console.WriteLine("");
 
         if(rpsUser == "rock" && rpsRan == "scissors" )
         {
-            Console.WriteLine("Computer Input: Scissors. User Input: Rock");
-            Console.WriteLine("You win!");
+            Console.WriteLine("User: Rock vs. Bot: Scissors");
+            Console.WriteLine("Winner!");
         }
         else if(rpsUser == "scissors" && rpsRan == "rock")
         {
-            Console.WriteLine("Computer Input: Rock. User Input: Scissors"); 
-            Console.WriteLine("You lost!");
+            Console.WriteLine("User: Scissors vs. Bot: Rock"); 
+            Console.WriteLine("Defeat!");
         }
         else if(rpsUser == "paper" && rpsRan == "scissors")
         {
-            Console.WriteLine("Computer Input: Scissors. User Input: Paper");
-            Console.WriteLine("You lost!");
+            Console.WriteLine("User: Paper vs. Bot: Scissors");
+            Console.WriteLine("Defeat!");
         }
         else if(rpsUser == "scissors" && rpsRan == "paper")
         {
-            Console.WriteLine("Computer Input: Paper. User Input: Scissors"); 
-            Console.WriteLine("You win!"); 
+            Console.WriteLine("User: Scissors vs. Bot: Paper"); 
+            Console.WriteLine("Winner!"); 
         }
         else if(rpsUser == rpsRan)
+        //draw case
         {
-            Console.WriteLine("It's a draw!"); 
+            Console.WriteLine("It's a Draw!"); 
             if(rpsUser == "scissors")
             {   
-                Console.WriteLine("Computer Input: Scissors. User Input: Scissors"); 
+                Console.WriteLine("User: Scissors vs. Bot: Scissors "); 
             }
             else if(rpsUser == "paper")
             {
-                Console.WriteLine("Computer Input: Paper. User Input: Paper"); 
+                Console.WriteLine("User: Paper vs. Bot: Paper "); 
             }
             else 
             {
-                Console.WriteLine("Computer Input: Rock. User Input: Rock"); 
+                Console.WriteLine("User: Rock vs. Bot: Rock "); 
             }
             
         }
         else if(rpsUser == "rock" && rpsRan == "paper")
         {
-            Console.WriteLine("Computer Input: Paper. User Input: Rock."); 
-            Console.WriteLine("You lost!"); 
+            Console.WriteLine("User: Rock vs. Bot: Paper "); 
+            Console.WriteLine("Defeat!"); 
         }
         else if(rpsUser == "paper" && rpsRan == "rock")
         {
-            Console.WriteLine("Computer Input: Rock. User Input: Paper."); 
-            Console.WriteLine("You win!"); 
+            Console.WriteLine("User: Paper vs. Bot: Rock"); 
+            Console.WriteLine("Winner!"); 
         }
     }
-
 }
 class UserInterface
 {
